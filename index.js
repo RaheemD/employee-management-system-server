678d2b4b22bbf29c13349d3c07ae320439549cfd
RaheemD <165933165+RaheemD@users.noreply.github.com>
2024-05-12 16:51:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import { adminRouter } from "./Routes/AdminRoute.js";
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*', // Allows all domains to make requests
     methods: ["GET", "POST", "PUT","DELETE"],
     credentials: true
   }));
