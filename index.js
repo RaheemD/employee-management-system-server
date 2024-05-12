fcbb062621d42553774634162365523e4676c3a4
RaheemD <165933165+RaheemD@users.noreply.github.com>
2024-05-12 16:17:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use('/auth', adminRouter);
 app.use(express.static('Public'))
 
-app.listen(3000, () => {
-    console.log("Server is running")
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
